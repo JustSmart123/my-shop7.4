6214813ec2674609b7144d611b4b68e71b9bfddf
Liliya Sviatenko <123566371+Liliya-ivanovna@users.noreply.github.com>
2025-02-10 12:57:01 +0200

    update

--- my-shop7.4/src/App.vue
+++ my-shop7.4/src/App.vue
@@ -1,6 +1,7 @@
 
 <template>
   <div id="app">
+    <HeaderComponent />
     <ProductList />
     <FooterComponent />
   </div>
@@ -8,10 +9,12 @@
 
 <script>
 import ProductList from './components/ProductList.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   components: {
+    HeaderComponent,
     ProductList,
     FooterComponent,
   },
